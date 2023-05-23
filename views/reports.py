cbd96396d089f489992794f06b94bc463bda95fd
LeChat76 <cedrik76@msn.com>
2023-05-23 12:11:57 +0200

    ajout du memory_usage dans le rapport

--- views/reports.py
+++ views/reports.py
@@ -1,18 +1,24 @@
-import datetime, sys
-from constantes import BRUTEFORCE_LOG_FILE
+import datetime, sys, psutil
 sys.path.append("..")
+from functions import getMemoryUsage
 
 def optimizedReport(result):
+    memoryUsed = getMemoryUsage()
     print("Choix des actions à acheter pour un meilleur rendement : \n" + str(result[0]) + ".")
     print("Cout des achats : " + str(format(result[2], '.2f') + "€.")  )
     print("Gain avec cette combinaison : " + str(format(result[1], '.2f') + "€."))
     print("Duree de traitement :" ,result[3])
+    print("Utilisation de la mémoire : " + format(memoryUsed, '.2f') + "Mo.")
+
+
 
 def bruteforceReport(actions, actionsCost, bestGain, start):
+    memoryUsed = getMemoryUsage()
     print("Choix des actions à acheter pour un meilleur rendement : \n" + str(actions[:-2]) + ".")
     print("Cout des achats : " + str(actionsCost) + "€.")
     print("Gain avec cette combinaison : " + str(bestGain) + "€.")
     print("Duree de traitement : " + str(datetime.datetime.now() - start)[:10])
+    print("Utilisation de la mémoire : " + format(memoryUsed, '.2f') + "Mo.")
 
 def displayMsg(message):
     print(message)
