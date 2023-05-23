286e63aa446bbba48d88e163a0598351fde33e87
LeChat76 <cedrik76@msn.com>
2023-05-23 17:06:27 +0200

    correction fichier des constantes

--- constantes.py
+++ constantes.py
@@ -5,6 +5,3 @@ NB_ACTIONS = 20
 COMBINATIONS_ACTIONS_LIST = []
 PROFITS_ACTIONS_LIST = []
 MAX_INVEST = 500
-OPTIMIZED_LOG_FILE = '..\datas\memory_profiler_optimized.log'
-OPTIMIZED2_LOG_FILE = '..\datas\memory_profiler_optimized2.log'
-BRUTEFORCE_LOG_FILE = '..\datas\memory_profiler_bruteforce.log'
