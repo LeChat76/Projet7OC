8f7c944ee571ce1ab1195c8c31a3999c9897e532
LeChat76 <cedrik76@msn.com>
2023-05-23 16:32:07 +0200

    modification actions.py pour ignorer cost=0

--- models/actions.py
+++ models/actions.py
@@ -2,13 +2,17 @@ class porteFolio:
     def __init__(self, name, cost, profit, actionsContainFloat): 
         self.name = name
         self.cost = float(cost)
-        self.profit = float(profit)
+        # if cost <= 0 (so I guess it's a mistake), I modify cost and gain by 0 to bypass this action
+        if self.cost <= 0:
+            self.cost = 0
+            self.profit = 0
+        else:
+            self.profit = float(profit)
         if actionsContainFloat:
             self.gain = self.cost * (self.profit / 10000)
         else:
             self.gain = self.cost * (self.profit / 100 )
         self.ratio = (self.cost * (self.profit / 100)) + self.profit
-        # self.ratio = self.profit + (self.cost * self.profit)
 
     def __lt__(self, nextObj):
         return self.ratio < nextObj.ratio
